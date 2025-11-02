73d2390bfaf7d3696f258ed8e040e09296635c7e
Rishabh90501 <rishabh.salian@outlook.com>
2025-11-03 02:42:06 +0530

    Data Transformation Completed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,11 +1,16 @@
 import os 
 import sys
-from src.exception import CustomException
-from src.logger import logging
+
 import pandas as pd
 import numpy as np
-
 from dataclasses import dataclass
+
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+from src.exception import CustomException
+from src.logger import logging
+
 
 @dataclass
 class DataIngestionConfig:
@@ -51,4 +56,7 @@ class DataIngestion:
         
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data, valid_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data, valid_data)
