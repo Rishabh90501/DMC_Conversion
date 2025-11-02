9639db7440a7df0fa2bdf2f28109b0bbf62dcb0c
Rishabh90501 <rishabh.salian@outlook.com>
2025-11-03 01:32:31 +0530

    Update Data Ingestion File

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,54 @@
+import os 
+import sys
+from src.exception import CustomException
+from src.logger import logging
+import pandas as pd
+import numpy as np
+
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path: str = os.path.join('artifacts', 'train.csv')
+    test_data_path: str = os.path.join('artifacts', 'test.csv')
+    valid_data_path: str = os.path.join('artifacts', 'valid.csv')
+    raw_data_path: str = os.path.join('artifacts', 'data.csv')
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        logging.info("Data Ingestion started")
+        try:
+            df = pd.read_csv('notebook/data/dm_data.csv')
+            logging.info("Dataset read as dataframe")
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+            logging.info("Raw data saved")
+
+            logging.info("Initiating data splitting into training, testing and validation sets")
+            train_set, valid_set, test_set = np.split(df.sample(frac=1),[int(0.6*len(df)), int(0.8*len(df))])
+
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False)
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False)
+            valid_set.to_csv(self.ingestion_config.valid_data_path, index=False)
+            
+            logging.info("Train, Test and Validation data saved")
+            logging.info("Data ingestion completed")
+
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path,
+                self.ingestion_config.valid_data_path
+            )
+
+        except Exception as e:
+            logging.error("Error occurred during data ingestion")
+            raise CustomException(e, sys)
+        
+if __name__ == "__main__":
+    obj = DataIngestion()
+    obj.initiate_data_ingestion()
